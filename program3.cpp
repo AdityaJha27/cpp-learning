16fc5477b7b2116062c108df0ab2a524725d9ee2
AdityaJha27 <adityajha8276@gmail.com>
2026-05-18 03:30:17 +0530

    variables and comments basics in C++

---
+++ program3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+// This programm is created by Aditya
+/*
+This 
+is 
+multi 
+line 
+comment
+ */
+int main(){
+    int sum = 5;
+    cout<<"Hello World!"<< sum << endl;
+    return 0;
+}
